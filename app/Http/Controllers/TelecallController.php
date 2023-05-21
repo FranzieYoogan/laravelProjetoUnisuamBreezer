22946bab6c61281a4e2785a3e006d3c27b463bd9
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-05-21 18:54:19 -0300

    bugsFixed

--- app/Http/Controllers/TelecallController.php
+++ app/Http/Controllers/TelecallController.php
@@ -16,22 +16,25 @@ use Symfony\Contracts\EventDispatcher\Event;
 
 class TelecallController extends Controller
 {
-    public function home() {
+    public function home()
+    {
         return view('layouts.home');
     }
 
-    public function index(Request $request) {  
+    public function index(Request $request)
+    {
         $value = $request->input('checkbox');
 
-        return view('welcome',['value' =>$value]);
+        return view('welcome', ['value' => $value]);
     }
 
-    public function storeFaleConosco(Request $request) {
-        
-        
+    public function storeFaleConosco(Request $request)
+    {
+
+
         $fale_conosco = new Fale_conosco;
-        
-        
+
+       
 
 
         $fale_conosco->nome = $request->nome;
@@ -41,131 +44,139 @@ class TelecallController extends Controller
         $fale_conosco->mensagem = $request->mensagem;
         $fale_conosco->checkbox = $request->checkbox;
 
-        if($request->checkbox && $request  && $request->nome && $request->nome != $fale_conosco->nome  && 
-        $request->empresa && $request->empresa != $fale_conosco->empresa   && 
-        $request->telefone && $request->telefone != $fale_conosco->telefone && $request->email && 
-        $request->email != $fale_conosco->email   && $request->mensagem   ) {
-            $fale_conosco->save(); 
-            return redirect('/entraremosemcontato');
-        
-        }
-        elseif($request->checkbox && (!$request->nome || !$request->empresa|| 
-        !$request->telefone || !$request->email || !$request->mensagem) ){
+        if ((Fale_conosco::where('nome',$request->nome)->exists()) OR
+         (Fale_conosco::where('empresa',$request->empresa)->exists()) 
+         OR (Fale_conosco::where('telefone',$request->telefone)->exists()) 
+         OR (Fale_conosco::where('email',$request->email)->exists()) 
+          )
+            
+         {
            
+            return redirect('/dadosexistentes');
+         
+           
+        } elseif (
+            $request->checkbox and (!$request->nome or !$request->empresa or
+                !$request->telefone or !$request->email or !$request->mensagem)
+        ) {
+
             return redirect('/campovazio');
 
-        }
-        elseif(!$request->checkbox && (!$request->nome || !$request->empresa|| 
-        !$request->telefone || !$request->email || !$request->mensagem) ) {
+        } elseif (
+            !$request->checkbox and (!$request->nome or !$request->empresa or
+                !$request->telefone or !$request->email or !$request->mensagem)
+        ) {
             return redirect('/errors2');
-        }
-     
-
-        elseif(!$request->checkbox && ($request->nome || $request->empresa|| 
-        $request->telefone || $request->email || $request->mensagem)) {
+        } elseif (
+            !$request->checkbox and ($request->nome or $request->empresa or
+                $request->telefone or $request->email or $request->mensagem)
+        ) {
             return redirect('/error');
 
+         } elseif(($request->checkbox) and ($request->nome)  and
+         ($request->empresa) and 
+         ($request->telefone) and   ($request->email) 
+          and ($request->mensagem))  {
+
+            $fale_conosco->save();
+            return redirect('/entraremosemcontato');
+          
+             
+            
+          }
         }
-       
-           
-     elseif(($fale_conosco->nome == $request->nome || $fale_conosco->empresa == $request->empresa
-    || $fale_conosco->telefone == $request->telefone || $fale_conosco->email == $request->email)) {
-        return redirect('/dadosexistentes');
-
     
-    
-    }
-}
 
-    public function error() {
+
+         
+    
+
+
+    public function error()
+    {
         return view('error');
     }
 
-    public function errors2() {
+    public function errors2()
+    {
         return view('errors2');
     }
 
-    public function dadosExistentes() {
-        return view ('dadosexistentes');
+    public function dadosExistentes()
+    {
+        return view('dadosexistentes');
     }
 
-    public function campoVazio() {
+    public function campoVazio()
+    {
         return view('campoVazio');
     }
 
-    public function entraremosEmContato() {
+    public function entraremosEmContato()
+    {
         return view('entraremosemcontato');
     }
 
-    public function wholesale() {
+    public function wholesale()
+    {
         return view('wholesale');
     }
 
-    public function institucional() {
+    public function institucional()
+    {
         return view('institucional');
     }
 
-    public function faq() {
+    public function faq()
+    {
         return view('faq');
     }
 
-    public function carreiras() {
-        
-        $carreiras = Vagas_aberta::all();
-        
-            
-        
+    public function carreiras()
+    {
 
-       
+        $carreiras = Vagas_aberta::all();
+
+
+
+
+
         $search = request('query');
         $selectCarreiras = request('selectCarreiras');
-       
 
-        if($search) {
+
+        if ($search) {
             $carreiras = Vagas_aberta::where([
-                ['vagas_nome','like','%'.$search.'%']
+                ['vagas_nome', 'like', '%' . $search . '%']
             ])->get();
 
-         
-            
+
+
             $carreirasCount = $carreiras->count();
 
 
-        }  else {
+        } else {
             $carreiras;
             $carreirasCount = $carreiras->count();
         }
 
-        
-
-        return view('carreiras',['carreiras' => $carreiras,'carreirasCount' =>$carreirasCount,'search'=>$search]);
-    
-        }
 
 
-
-    
-
-
-       
-            
-       
-
-    
- 
-
+        return view('carreiras', ['carreiras' => $carreiras, 'carreirasCount' => $carreirasCount, 'search' => $search]);
 
     }
-       
-    
 
-    
-    
-    
-      
-     
-    
 
-        
-    
 
+
+
+
+
+
+
+
+
+
+
+
+}
